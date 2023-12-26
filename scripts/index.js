0790ce6f70a41077f1efb6cbc22ac96cbf7bf5f3
ChronoWave <ChronoWave@yandex.ru>
2023-12-25 22:16:43 -0800

    v1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+document.querySelector(".profile__add-button").onclick = function() {
+  document.querySelector(".popup_type_new-card").classList.add("popup_is-opened")
+}
 
-// @todo: DOM узлы
+document.querySelector(".popup__close").onclick = function() {
+  document.querySelector(".popup_type_new-card").classList.remove("popup_is-opened")
+}
 
-// @todo: Функция создания карточки
+document.querySelector(".places__list").onclick = function(event) {
+  const cardDelete = event.target.closest(".places__item")
+  cardDelete.remove()
+}
 
-// @todo: Функция удаления карточки
+const cardsTemplate = document.querySelector("#card-template").content;
+const cardsContainer = document.querySelector(".places__list");
 
-// @todo: Вывести карточки на страницу
+const cardsData = initialCards.map(function (item) {
+  return {
+    name: item.name,
+    link: item.link
+  };
+});
+
+function renderCards({name, link}) {
+  const cardsContent = cardsTemplate.querySelector(".places__item").cloneNode(true);
+  cardsContent.querySelector(".card__title").textContent = name;
+  cardsContent.querySelector(".card__image").src = link;
+  cardsContainer.prepend(cardsContent);
+}
+
+function render() {
+  cardsData.forEach(renderCards);
+}
+
+render();
